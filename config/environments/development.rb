d8059b3ee491a9f0d5367c70f2438d3a46963fb7
Ikeda Takanao <ttttt.iiiii.tip@gmail.com>
2017-12-18 01:34:47 +0000

    mail setting

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,17 @@ Rails.application.configure do
   
   #devise setting
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp4.gmoserver.jp",
+    :port => 587,
+    :user_name => "info@starlistz.com",
+    :password => "$MA8J938",
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
