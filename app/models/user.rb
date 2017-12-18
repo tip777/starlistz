633df531f7cfd0e351b9dfcf3f14823cfa8d90e8
Ikeda Takanao <ttttt.iiiii.tip@gmail.com>
2017-12-18 01:28:38 +0000

    twitter認証

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,24 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable, :timeoutable, :omniauthable, omniauth_providers: [:twitter]
+         
+    def self.from_omniauth(auth)
+        where(provider: auth["provider"], uid: auth["uid"]).first_or_create do |user|
+            user.provider = auth["provider"]
+            user.uid = auth["uid"]
+            user.name = auth["info"]["nickname"]
+        end
+    end
+
+    def self.new_with_session(params, session)
+        if session["devise.user_attributes"]
+            new(session["devise.user_attributes"]) do |user|
+                user.attributes = params
+                user.valid?
+            end
+        else
+            super
+        end
+    end
+    
 end
