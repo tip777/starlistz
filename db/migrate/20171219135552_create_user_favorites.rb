883044dc25f80ca7fe7e84e0137106248bc3bd68
Ikeda Takanao <ttttt.iiiii.tip@gmail.com>
2017-12-20 09:50:08 +0000

    migrateエラー修正

--- db/migrate/20171219135552_create_user_favorites.rb
+++ db/migrate/20171219135552_create_user_favorites.rb
@@ -1,11 +1,14 @@
 class CreateUserFavorites < ActiveRecord::Migration[5.0]
   def change
     create_table :user_favorites do |t|
-      t.references :favoriting, foreign_key: true
-      t.references :fovorited, foreign_key: true
-
+      t.integer :favoriter_id
+      t.integer :favorited_id
       t.timestamps null: false
-      t.index [:favoriting_id, :favorited_id], unique: true
     end
+      # t.index [:favoriter_id, :favorited_id], unique: true
+      add_index :user_favorites, :favoriter_id
+      add_index :user_favorites, :favorited_id
+      add_index :user_favorites, [:favoriter_id, :favorited_id], unique: true
+    
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,99 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171218010826) do
+ActiveRecord::Schema.define(version: 20171219140836) do
 
-  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "item_services", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+    t.integer  "music_service_id"
+    t.integer  "list_item_id"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.index ["list_item_id"], name: "index_item_services_on_list_item_id", using: :btree
+    t.index ["music_service_id"], name: "index_item_services_on_music_service_id", using: :btree
+  end
+
+  create_table "list_contents", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+    t.integer  "list_id"
+    t.integer  "list_item_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["list_id"], name: "index_list_contents_on_list_id", using: :btree
+    t.index ["list_item_id"], name: "index_list_contents_on_list_item_id", using: :btree
+  end
+
+  create_table "list_favorites", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+    t.integer  "list_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["list_id"], name: "index_list_favorites_on_list_id", using: :btree
+    t.index ["user_id"], name: "index_list_favorites_on_user_id", using: :btree
+  end
+
+  create_table "list_items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+    t.string   "artist"
+    t.string   "song"
+    t.integer  "favorite"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "lists", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+    t.integer  "user_id"
+    t.string   "name"
+    t.string   "description"
+    t.integer  "price"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+    t.index ["user_id"], name: "index_lists_on_user_id", using: :btree
+  end
+
+  create_table "music_services", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+    t.string   "name"
+    t.text     "url",        limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
+
+  create_table "purchases", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+    t.date     "order_date"
+    t.integer  "user_id"
+    t.integer  "list_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["list_id"], name: "index_purchases_on_list_id", using: :btree
+    t.index ["user_id"], name: "index_purchases_on_user_id", using: :btree
+  end
+
+  create_table "user_favorites", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+    t.integer  "favoriter_id"
+    t.integer  "favorited_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["favorited_id"], name: "index_user_favorites_on_favorited_id", using: :btree
+    t.index ["favoriter_id", "favorited_id"], name: "index_user_favorites_on_favoriter_id_and_favorited_id", unique: true, using: :btree
+    t.index ["favoriter_id"], name: "index_user_favorites_on_favoriter_id", using: :btree
+  end
+
+  create_table "user_profiles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+    t.integer  "user_id"
+    t.text     "description",         limit: 65535
+    t.text     "insta_url",           limit: 65535
+    t.text     "tw_url",              limit: 65535
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
+    t.index ["user_id"], name: "index_user_profiles_on_user_id", using: :btree
+  end
+
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -37,4 +127,14 @@ ActiveRecord::Schema.define(version: 20171218010826) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "item_services", "list_items"
+  add_foreign_key "item_services", "music_services"
+  add_foreign_key "list_contents", "list_items"
+  add_foreign_key "list_contents", "lists"
+  add_foreign_key "list_favorites", "lists"
+  add_foreign_key "list_favorites", "users"
+  add_foreign_key "lists", "users"
+  add_foreign_key "purchases", "lists"
+  add_foreign_key "purchases", "users"
+  add_foreign_key "user_profiles", "users"
 end
