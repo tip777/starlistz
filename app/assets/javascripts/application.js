0da6bcb2bb0a2d4a610642fa873ce8746e4326fb
Ikeda Takanao <info@starlistz.com>
2017-12-22 00:40:39 +0900

    css javascript 対応

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,42 @@
 // about supported directives.
 //
 //= require jquery
+//= require bootstrap-sprockets
 //= require jquery_ujs
 //= require turbolinks
+//= require select2
 //= require_tree .
+$(function() {
+    var h;
+    //スマホの場合padding-top足す
+    if(!window.matchMedia('(min-width:736px)').matches){
+        h = $('#header').height() + 100;
+    }else{
+        h = $('#header').height();
+    }
+    $('#page-wrapper').css('padding-top', h);
+});
+//= require turbolinks
+//= require_tree .
+
+$(function() {
+    //header padding-top
+    // var h;
+    // h = $('#header').height();
+    // $('#page-wrapper').css('padding-top', h);
+
+    //select2 option
+    $(".js-multiple").select2({
+        width:      200
+    });
+
+    $(".js-hide-search").select2({
+        minimumResultsForSearch: Infinity,
+        width:      150
+    });
+
+    $('.genre-search').change(function() {
+        console.log("yeah man");
+    });
+
+});
