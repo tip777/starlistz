d01a469eedacd304f3c2710d70db8741c66e8aca
Ikeda Takanao <info@starlistz.com>
2017-12-21 21:49:31 +0900

    omniauth 修正

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,13 +1,12 @@
-sclass OmniauthCallbacksController < ApplicationController
-    def twitter
-        @user = User.from_omniauth(request.env["omniauth.auth"].except("extra"))
+class OmniauthCallbacksController < ApplicationController
+  def twitter
+      @user = User.from_omniauth(request.env["omniauth.auth"].except("extra"))
 
-        if @user.persisted?
-          flash.notice = 'ログインしました。'
+      if @user.persisted?
           sign_in_and_redirect @user
-        else
+      else
           session["devise.user_attributes"] = @user.attributes
           redirect_to new_user_registration_url
-        end
-    end
+      end
+  end
 end
