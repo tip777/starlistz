de9a2f313fb59e1e35e441a2ac4af1c8f48d7b30
takanao <ttttt.iiiii.tip@gmail.com>
2018-01-05 00:50:47 +0900

    profile update nameだけOK

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,6 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   def new
@@ -59,7 +59,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
     # devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
     devise_parameter_sanitizer.permit(:account_update) do |params|
       params.permit(:email, :password, :password_confirmation, :current_password, :name,
-                    user_profiles: [:id, :description, :insta_url, :tw_url ])
+                    user_profile: [:id, :description, :insta_url, :tw_url ])
     end
   end
 
@@ -67,6 +67,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # You can overwrite this method in your own RegistrationsController.
   def update_resource(resource, params)
     resource.update_without_password(params)
+  end
+
+  #マイページ更新後に編集画面表示
+  def after_update_path_for(resource)
+    edit_user_registration_path
   end
 
   # The path used after sign up.
