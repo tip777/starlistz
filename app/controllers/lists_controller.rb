7759372287bc9f924f5cd74ee365d4441cbf179b
takanao <ttttt.iiiii.tip@gmail.com>
2018-01-10 00:25:05 +0900

    list edit list_item表示

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,8 +2,14 @@ class ListsController < ApplicationController
   def new
     @list = List.new
   end
-  
+
   def edit
     # @list = List.new
+    set_list
+  end
+
+  private
+  def set_list
+    @list = List.find(params[:id])
   end
 end
