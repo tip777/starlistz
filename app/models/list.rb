f0fb1d32ee9849c5f1ce6f90e423671005f4ad9a
takanao <ttttt.iiiii.tip@gmail.com>
2018-02-08 00:29:17 +0900

    list.rb has_many usersコメント

--- app/models/list.rb
+++ app/models/list.rb
@@ -6,14 +6,14 @@ class List < ApplicationRecord
   accepts_nested_attributes_for :tracks, allow_destroy: true
 
   has_many :purchases, dependent: :destroy
-  has_many :users, through: :users
+  # has_many :users, through: :users
 
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
   #gem acts-as-taggable-on タグ機能
   acts_as_ordered_taggable_on :maingenres, :othergenres
-  
+
   #validate
   validates :title, presence: true
   validates :title, presence: true
