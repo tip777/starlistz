82387a13b117c95ed5290b380f9979408d934dbc
Ikeda Takanao <ttttt.iiiii.tip@gmail.com>
2018-02-20 11:25:37 +0000

    home chart ページとコントローラー修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -13,32 +13,32 @@ class HomeController < ApplicationController
   def genre
   end
 
-  # def chart
-  #   #パラメータからリスト抽出
-  #   array = Array.new
-  #
-  #   if params[:genre][0] == "all" #ジャンルが空だったら
-  #     if params[:sort] == "ranking" #ランキングの場合
-  #       @genre_list = List.eager_load(:list_favorites).group(:list_id).order('count(list_id) desc')
-  #     else  # 新着の場合
-  #       @genre_list = List.order(:created_at)
-  #     end
-  #
-  #   else
-  #
-  #     params[:genre].each_with_index do |value, i| #ジャンルを配列に
-  #       array.push(value)
-  #     end
-  #     genrelist = List.tagged_with(array).pluck(:id)#ジャンルの対象のユーザーのIDの一覧取得
-  #
-  #     if params[:sort] == "ranking" #ランキングの場合
-  #       @genre_list = List.eager_load(:list_favorites).where(id: genrelist).group(:list_id).order('count(list_id) desc')
-  #     else  # 新着の場合
-  #       @genre_list = List.where(id: genrelist).order(:created_at)
-  #     end
-  #
-  #   end
-  # end
+  def chart
+    #パラメータからリスト抽出
+    array = Array.new
+    binding.pry
+    if params[:genre][0] == "all" #ジャンルが空だったら
+      if params[:sort] == "ranking" #ランキングの場合
+        @genre_list = List.eager_load(:list_favorites).group(:list_id).order('count(list_id) desc')
+      else  # 新着の場合
+        @genre_list = List.order(:created_at)
+      end
+  
+    else
+  
+      params[:genre].each_with_index do |value, i| #ジャンルを配列に
+        array.push(value)
+      end
+      genrelist = List.tagged_with(array).pluck(:id)#ジャンルの対象のユーザーのIDの一覧取得
+  
+      if params[:sort] == "ranking" #ランキングの場合
+        @genre_list = List.eager_load(:list_favorites).where(id: genrelist).group(:list_id).order('count(list_id) desc')
+      else  # 新着の場合
+        @genre_list = List.where(id: genrelist).order(:created_at)
+      end
+  
+    end
+  end
 
   def set_genre
     # @base_genre = ListGenre.where(main: 1)
@@ -56,7 +56,7 @@ class HomeController < ApplicationController
     # @tag["メインジャンル"] = hash
     # @tag["その他"] = hash2
 
-    mainno = ActsAsTaggableOn::Tagging.where(context: "maingenres").pluck(:tag_id)
+    mainno = ActsAsTaggableOn::Tag.where(taggings_count: 0)
     otherno = ActsAsTaggableOn::Tagging.where(context: "othergenres").pluck(:tag_id)
     @tag = Hash.new{|h,k| h[k] = {} }
     @tag["メインジャンル"] = ActsAsTaggableOn::Tag.where(id: mainno).pluck(:name)
