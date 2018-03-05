93718b19d28d4ab5d54a8bb54011826c0585845d
takanao <ttttt.iiiii.tip@gmail.com>
2018-03-06 00:51:40 +0900

    プレイリスト表示方法等統一　lists/_list.html.haml

--- db/seeds.rb
+++ db/seeds.rb
@@ -97,13 +97,11 @@ $list.each do |tag|
   target.save
 end
 
-binding.pry
-
 list_itiran = List.all
 list_itiran.each_with_index do |tag, i|
   target = ActsAsTaggableOn::Tagging.new(tag_id: i+1, taggable_type: "List", taggable_id: i+1, context: "tags")
   target.save
-  
+
   if i.even?
     random = Random.new
     n = random.rand(1..50)
