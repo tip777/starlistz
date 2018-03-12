0b1411bb574792fff7ac01bd805d054c02b5002d
Ikeda Takanao <ttttt.iiiii.tip@gmail.com>
2018-03-12 09:29:21 +0000

    ransack 複数キーワード　検索

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,39 +4,16 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def search_header
-      # @q = User.ransack(params[:q])
-      # @search_user = @q.result(distinct: true)
-      #
-      # @q2 = List.ransack(params[:q])
-      # @search_list = @q2.result(distinct: true)
-
-      # binding.pry
-      if params[:q] != nil
-        keword = params[:q][:name_cont]
-        @q = User.ransack(name_cont: keword)
+      if params[:q] != nil and params[:q] != ""
+        # key_words = params[:q][:q].split(/[\p{blank}\s]+/) # スペースがあったら分ける
+        key_words = params[:q].split(/[\p{blank}\s]+/)
+        @q = User.ransack(name_cont_any: key_words)
         @search_user = @q.result(distinct: true)
-
-        @q2 = List.ransack(title_cont: keword)
-        @search_list = @q2.result(distinct: true)
+        
+        @search_list = List.ransack(title_cont_any: key_words).result(distinct: true)
       else
-        @q = User.ransack(params[:q])
+        @q = User.search(params[:q])
       end
-
-      # binding.pry
-      # key_words = params[:q][:name_cont].split(/[\p{blank}\s]+/) # スペースがあったら分ける
-      # grouping_hash = key_words.reduce({}){|hash, word| hash.merge(word => { name_cont: word })}
-      # @q = User.ransack({ combinator: 'and', groupings: grouping_hash, s: 'name desc' })
-      # @search_user = @q.result(distinct: true)
-      #
-      # grouping_hash2 = key_words.reduce({}){|hash, word| hash.merge(word => { title_cont: word })}
-      # @search_list = List.ransack({ combinator: 'and', groupings: grouping_hash, s: 'title desc' }).result(distinct: true)
-
-      # binding.pry
-      # key_words = params[:q][:q].split(/[\p{blank}\s]+/) # スペースがあったら分ける
-      # @q = User.ransack(name_cont: key_words)
-      # @search_user = @q.result(distinct: true)
-      #
-      # @search_list = List.ransack(title_cont: key_words).result(distinct: true)
   end
 
   def set_list_genre
