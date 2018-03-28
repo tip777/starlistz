3f1c3e2325e51d9bc262641bfa02c8c9d521fe3c
Ikeda Takanao <ttttt.iiiii.tip@gmail.com>
2018-03-28 04:34:23 +0000

    rememberableを削除 ログイン2日間保持

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
+        # :recoverable, :rememberable, :trackable, :validatable,
+        #:rememberableを削除
+        :recoverable, :trackable, :validatable,
         # :confirmable, :timeoutable, :omniauthable, omniauth_providers: [:twitter]
         :timeoutable, :omniauthable, omniauth_providers: [:twitter] #cloud9用
 
