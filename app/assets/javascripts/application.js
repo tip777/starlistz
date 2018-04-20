b5ea3d960dd796605dfe946dfb4d812f1859af89
ikeda_bit <andpropose@gmail.com>
2018-04-20 20:22:03 +0900

    スクロール修正

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -65,25 +65,11 @@ $(document).on('turbolinks:load', function() {
   //strにhogeを含む場合の処理
   }
   
-  
+
+
+
   var windowWidth = window.innerWidth;
-  if (windowWidth > 768) {
   
-    $('.menu-trigger').click(function(e){
-      $('+ .collapse-down-menu', this).slideToggle(300,"easeOutQuad");
-    })
-  }else{
-    $('.menu-trigger').click(function(e){
-      var opened = $('+ .collapse-down-menu', this);
-      if(opened.hasClass("open")){
-        opened.removeClass("open");
-      } else{
-        $('.collapse-down-menu').removeClass("open");
-        opened.toggleClass("open");
-        return true;
-      }
-    });
-  }
 
 });
 
@@ -205,8 +191,8 @@ opened.removeClass("open");
 $(function(){
 
 //グローバル変数
-var nowModalSyncer = null ;		//現在開かれているモーダルコンテンツ
-var modalClassSyncer = "modal-syncer" ;		//モーダルを開くリンクに付けるクラス名
+var nowModalSyncer = null ;   //現在開かれているモーダルコンテンツ
+var modalClassSyncer = "modal-syncer" ;   //モーダルを開くリンクに付けるクラス名
 
 //モーダルのリンクを取得する
 var modals = document.getElementsByClassName( modalClassSyncer ) ;
@@ -214,86 +200,86 @@ var modals = document.getElementsByClassName( modalClassSyncer ) ;
 //モーダルウィンドウを出現させるクリックイベント
 for(var i=0,l=modals.length; l>i; i++){
 
-	//全てのリンクにタッチイベントを設定する
-	modals[i].onclick = function(){
+  //全てのリンクにタッチイベントを設定する
+  modals[i].onclick = function(){
 
-		//ボタンからフォーカスを外す
-		this.blur() ;
+    //ボタンからフォーカスを外す
+    this.blur() ;
 
-		//ターゲットとなるコンテンツを確認
-		var target = this.getAttribute( "data-target" ) ;
+    //ターゲットとなるコンテンツを確認
+    var target = this.getAttribute( "data-target" ) ;
 
-		//ターゲットが存在しなければ終了
-		if( typeof( target )=="undefined" || !target || target==null ){
-			return false ;
-		}
+    //ターゲットが存在しなければ終了
+    if( typeof( target )=="undefined" || !target || target==null ){
+      return false ;
+    }
 
-		//コンテンツとなる要素を取得
-		nowModalSyncer = document.getElementById( target ) ;
+    //コンテンツとなる要素を取得
+    nowModalSyncer = document.getElementById( target ) ;
 
-		//ターゲットが存在しなければ終了
-		if( nowModalSyncer == null ){
-			return false ;
-		}
+    //ターゲットが存在しなければ終了
+    if( nowModalSyncer == null ){
+      return false ;
+    }
 
-		//キーボード操作などにより、オーバーレイが多重起動するのを防止する
-		if( $( "#modal-overlay" )[0] ) return false ;		//新しくモーダルウィンドウを起動しない
-		//if($("#modal-overlay")[0]) $("#modal-overlay").remove() ;		//現在のモーダルウィンドウを削除して新しく起動する
+    //キーボード操作などにより、オーバーレイが多重起動するのを防止する
+    if( $( "#modal-overlay" )[0] ) return false ;   //新しくモーダルウィンドウを起動しない
+    //if($("#modal-overlay")[0]) $("#modal-overlay").remove() ;   //現在のモーダルウィンドウを削除して新しく起動する
 
-		//オーバーレイを出現させる
-		$( "body" ).append( '<div id="modal-overlay"></div>' ) ;
-		$( "#modal-overlay" ).fadeIn( "fast" ) ;
+    //オーバーレイを出現させる
+    $( "body" ).append( '<div id="modal-overlay"></div>' ) ;
+    $( "#modal-overlay" ).fadeIn( "fast" ) ;
 
-		//コンテンツをセンタリングする
-		centeringModalSyncer() ;
+    //コンテンツをセンタリングする
+    centeringModalSyncer() ;
 
-		//コンテンツをフェードインする
-		$( nowModalSyncer ).fadeIn( "slow" ) ;
+    //コンテンツをフェードインする
+    $( nowModalSyncer ).fadeIn( "slow" ) ;
 
-		//[#modal-overlay]、または[#modal-close]をクリックしたら…
-		$( "#modal-overlay,#modal-close" ).unbind().click( function() {
+    //[#modal-overlay]、または[#modal-close]をクリックしたら…
+    $( "#modal-overlay,#modal-close" ).unbind().click( function() {
 
-			//[#modal-content]と[#modal-overlay]をフェードアウトした後に…
-			$( "#" + target + ",#modal-overlay" ).fadeOut( "fast" , function() {
+      //[#modal-content]と[#modal-overlay]をフェードアウトした後に…
+      $( "#" + target + ",#modal-overlay" ).fadeOut( "fast" , function() {
 
-				//[#modal-overlay]を削除する
-				$( '#modal-overlay' ).remove() ;
+        //[#modal-overlay]を削除する
+        $( '#modal-overlay' ).remove() ;
 
-			} ) ;
+      } ) ;
 
-			//現在のコンテンツ情報を削除
-			nowModalSyncer = null ;
+      //現在のコンテンツ情報を削除
+      nowModalSyncer = null ;
 
-		} ) ;
+    } ) ;
 
-	}
+  }
 
 }
 
-	//リサイズされたら、センタリングをする関数[centeringModalSyncer()]を実行する
-	$( window ).resize( centeringModalSyncer ) ;
+  //リサイズされたら、センタリングをする関数[centeringModalSyncer()]を実行する
+  $( window ).resize( centeringModalSyncer ) ;
 
-	//センタリングを実行する関数
-	function centeringModalSyncer() {
+  //センタリングを実行する関数
+  function centeringModalSyncer() {
 
-		//モーダルウィンドウが開いてなければ終了
-		if( nowModalSyncer == null ) return false ;
+    //モーダルウィンドウが開いてなければ終了
+    if( nowModalSyncer == null ) return false ;
 
-		//画面(ウィンドウ)の幅、高さを取得
-		var w = $( window ).width() ;
-		var h = $( window ).height() ;
+    //画面(ウィンドウ)の幅、高さを取得
+    var w = $( window ).width() ;
+    var h = $( window ).height() ;
 
-		//コンテンツ(#modal-content)の幅、高さを取得
-		// jQueryのバージョンによっては、引数[{margin:true}]を指定した時、不具合を起こします。
-//		var cw = $( nowModalSyncer ).outerWidth( {margin:true} ) ;
-//		var ch = $( nowModalSyncer ).outerHeight( {margin:true} ) ;
-		var cw = $( nowModalSyncer ).outerWidth() ;
-		var ch = $( nowModalSyncer ).outerHeight() ;
+    //コンテンツ(#modal-content)の幅、高さを取得
+    // jQueryのバージョンによっては、引数[{margin:true}]を指定した時、不具合を起こします。
+//    var cw = $( nowModalSyncer ).outerWidth( {margin:true} ) ;
+//    var ch = $( nowModalSyncer ).outerHeight( {margin:true} ) ;
+    var cw = $( nowModalSyncer ).outerWidth() ;
+    var ch = $( nowModalSyncer ).outerHeight() ;
 
-		//センタリングを実行する
-		$( nowModalSyncer ).css( {"left": ((w - cw)/2) + "px","top": ((h - ch)/2) + "px"} ) ;
+    //センタリングを実行する
+    $( nowModalSyncer ).css( {"left": ((w - cw)/2) + "px","top": ((h - ch)/2) + "px"} ) ;
 
-	}
+  }
 
 } ) ;
 
