24d122040b7df02838b1370bdff5d6ab7eabc5c1
Ikeda Takanao <ttttt.iiiii.tip@gmail.com>
2018-06-19 11:04:52 +0000

    heroku mail 設定変更

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   #devise setting
-  config.action_mailer.default_url_options = { host: host}
+  config.action_mailer.default_url_options = { host: 'star-stg.herokuapp.com'}
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
