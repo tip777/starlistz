376d7bf8c63b595a85826007ec7db713f40475aa
Ikeda Takanao <ttttt.iiiii.tip@gmail.com>
2018-07-24 05:42:00 +0000

    メール有効期限チェック用

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -132,7 +132,8 @@ Devise.setup do |config|
   # their account can't be confirmed with the token any more.
   # Default is nil, meaning there is no restriction on how long a user can take
   # before confirming their account.
-  config.confirm_within = 1.days #仮登録系のメールの期限は1日
+  # config.confirm_within = 1.days #仮登録系のメールの期限は1日
+  config.confirm_within = 5.minute 
 
   # If true, requires any email changes to be confirmed (exactly the same way as
   # initial account confirmation) to be applied. Requires additional unconfirmed_email
