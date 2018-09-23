73ec91157fc5261b9c01cde90864317954fed6ad
Ikeda Takanao <ttttt.iiiii.tip@gmail.com>
2018-09-23 13:04:11 +0000

    マイページのStripe連携のメッセージを変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
           is_account = is_stripe_account_id?(current_user)
           if is_account != true
             flash.now[:alert] = "<a id='stripe_connect' href='#{stripe_url_edit(current_user)}'>Stripe接続</a>　が完了していません。<br>
-                            完了しなければプレイリストを作成できません。".html_safe
+                            完了しなければプレイリストは公開されません。".html_safe
           end
         end
       end
