30366ab0e4932d6a67c6f894817fc1fcbf58e512
taka <ttttt.iiiii.tip@gmail.com>
2019-08-10 16:43:31 +0900

    Stripeの連携画面に説明を追加　helpページへのリンクも

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,4 +2,4 @@
 
 # Rails.application.config.session_store :cookie_store, key: '_repo_star_session'
 #ログイン保持用
-Rails.application.config.session_store :cookie_store, key: '_starlistz_session', expire_after: 2.day
+Rails.application.config.session_store :cookie_store, key: '_starlistz_session', expire_after: 1.month
