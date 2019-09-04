56bf52aad5da6a8b6eb73099f178f570598656d2
taka <ttttt.iiiii.tip@gmail.com>
2019-09-04 17:30:46 +0900

    パラメーターでプレイリストの内容も見えないようした

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,6 @@ module Starlistz
     config.time_zone = 'Asia/Tokyo' #日本時間に変更
     config.active_record.default_timezone = :local #DB に書かれる時刻を日本語に
     config.action_view.embed_authenticity_token_in_remote_forms = true#フォロー機能用
-    config.filter_parameters += [:first_name, :last_name, :first_name_kana, :last_name_kana, :birthday, :zipcode, :prefecture, :city, :address1, :address2, :phone_number]
+    config.filter_parameters += [:first_name, :last_name, :first_name_kana, :last_name_kana, :birthday, :zipcode, :prefecture, :prefecture_id, :city, :address1, :address2, :phone_number, :name, :email, :title, :message, :description, :price, :status, :news_letter, :list_sold, :stripe_charge_id, :uid, :provider, :nickname, :followers_count, :artist, :song, :description, :recommend, :row_order, :insta_url, :tw_url, :stripe_acct_id, :stripe_cus_id, :stripe_acct_secret, :secret_key, :identity, :tos_acceptance, :tos_acceptance_date]
   end
 end
